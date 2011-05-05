7365c61436b7552616b4b36dd55e7d7ef41f4b27
Ivan Plesko <ivan.plesko@gmail.com>
2011-05-05 15:13:04 +0200

    Register keywords as peweproxy module

--- static/peweproxy_keywords.js
+++ static/peweproxy_keywords.js
@@ -1,50 +1,49 @@
-var peweproxy_url_keywords = 'adaptive-proxy/key_words_call'
+peweproxy.register_module('keywords', function($) {
 
-var temp = function($) {
-	$(document).ready(function(){
-		$('div#peweproxy_icon_banner a.__peweproxy_keywords_button').click(function(){
-			$(this).blur();
-			renewSmallButton = false;
-			$('#peweproxy_keywords').hide().removeClass('hidden').fadeIn('fast');
-			$(peweproxy_addonIconBannerSelector).addClass('hidden');
-			$(smallButtonSelector).addClass('hidden');
-			peweproxy_get_keywords();
-			return false;
+	var peweproxy_url_keywords = 'adaptive-proxy/key_words_call'
+	
+	
+		$(document).ready(function(){
+			$('div#peweproxy_icon_banner a.__peweproxy_keywords_button').click(function(){
+				$(this).blur();
+				renewSmallButton = false;
+				$('#peweproxy_keywords').hide().removeClass('hidden').fadeIn('fast');
+				$(peweproxy.modules.bubble.peweproxy_addonIconBannerSelector).addClass('hidden');
+				$(peweproxy.modules.bubble.smallButtonSelector).addClass('hidden');
+				get_keywords();
+				return false;
+			});
+			$('div#peweproxy_keywords a.__peweproxy_keywords_closebutton').click(function(){
+				$(this).blur();
+				renewSmallButton = true;
+				$(peweproxy.modules.bubble.smallButtonSelector).removeClass('hidden');
+				$('#peweproxy_keywords').fadeOut('fast');
+				return false;
+			});
+			$('form#peweproxy_keywords_add_form').submit(function(){
+				post_data = $(this).serialize();
+				$.post(peweproxy_url_keywords+'?action=addKeyWord',post_data,function(data){
+					response = $.trim(data);
+					if (response == 'OK'){
+						get_keywords();
+						$('input#peweproxy_keywords_add_term').val('Slovo').focus();
+						$('input#peweproxy_keywords_add_type').val('Typ');
+						$('input#peweproxy_keywords_add_relevance').val('Váha');
+					} else {
+						alert('Pridávanie sa nepodarilo, skontrolujte správnosť údajov a akciu opakujte.');
+					}
+				})
+				return false;
+			});
 		});
-		$('div#peweproxy_keywords a.__peweproxy_keywords_closebutton').click(function(){
-			$(this).blur();
-			renewSmallButton = true;
-			$(smallButtonSelector).removeClass('hidden');
-			$('#peweproxy_keywords').fadeOut('fast');
-			return false;
-		});
-		$('form#peweproxy_keywords_add_form').submit(function(){
-			post_data = $(this).serialize();
-			$.post(peweproxy_url_keywords+'?action=addKeyWord',post_data,function(data){
-				response = $.trim(data);
-				if (response == 'OK'){
-					peweproxy_get_keywords();
-					$('input#peweproxy_keywords_add_term').val('Slovo').focus();
-					$('input#peweproxy_keywords_add_type').val('Typ');
-					$('input#peweproxy_keywords_add_relevance').val('Váha');
-				} else {
-					alert('Pridávanie sa nepodarilo, skontrolujte správnosť údajov a akciu opakujte.');
-				}
-			})
-			return false;
-		});
-	});
-} (adaptiveProxyJQuery);
-
-function peweproxy_keywords_edit(id){
-	var temp = function($) {
+	
+	
+	this.keywords_edit = function(id){
 		$('div#peweproxy_keywords_content table tr.row'+id+' span.static').hide();
 		$('div#peweproxy_keywords_content table tr.row'+id+' span.editable').show();
-	} (adaptiveProxyJQuery);
-}
-
-function peweproxy_keywords_delete(id){
-	var temp = function($) {
+	}
+	
+	this.keywords_delete = function(id){
 		if (confirm('Skutočne odstrániť kľúčové slovo '+$('div#peweproxy_keywords_content table tr.row'+id+' span.term').html()+'?')){
 			$.post(peweproxy_url_keywords+'?action=removeKeyWord',{
 				id:id
@@ -57,11 +56,9 @@ function peweproxy_keywords_delete(id){
 				}
 			});
 		}
-	} (adaptiveProxyJQuery);
-}
-
-function peweproxy_keywords_save(id){
-	var temp = function($) {
+	}
+	
+	this.keywords_save = function(id){
 		table = $('div#peweproxy_keywords_content table');
 		post_data = {
 			id:id,
@@ -79,35 +76,33 @@ function peweproxy_keywords_save(id){
 				alert('Pri ukladaní kľúčového slova nastala chyba. Skonstrolujte správnosť údajov.');
 			}
 		});
-	} (adaptiveProxyJQuery);
-}
-
-function peweproxy_get_keywords(){
-	var template = '<tr class="row[:id:] keyword_row">'+
-						'<td style="width: 179px">'+
-							'<span class="static term">[:term:]</span>'+
-							'<span class="editable"><input type="text" class="term" value="[:term:]"/></span>'+
-						'</td>'+
-						'<td style="width: 56px">'+
-							'<span class="static type">[:type:]</span>'+
-							'<span class="editable"><input type="text" class="type" value="[:type:]"/></span>'+
-						'</td>'+
-						'<td style="width: 43px">'+
-							'<span class="static relevance">[:relevance:]</span>'+
-							'<span class="editable"><input type="text" class="relevance" value="[:relevance:]"/></span>'+
-						'</td>'+
-						'<td style="width: 57px">[:source:]</td>'+
-						'<td style="width: 46px" class="buttons">'+
-							'<span class="static">'+
-								'<a href="#" onclick="peweproxy_keywords_edit(\'[:id:]\'); return false"><img src="'+peweproxy_keywords_htdocs_dir+'/edit_icon.png" alt="edit" /></a>'+
-							'</span>'+
-							'<span class="editable">'+
-								'<a href="#" onclick="peweproxy_keywords_save(\'[:id:]\'); return false;"><img src="'+peweproxy_keywords_htdocs_dir+'/ok_icon.png" alt="save" /></a>'+
-							'</span>'+
-							'<a href="#" onclick="peweproxy_keywords_delete(\'[:id:]\'); return false;"><img src="'+peweproxy_keywords_htdocs_dir+'/delete_icon.png" alt="delete" /></a>'+
-						'</td>'+
-					'</tr>';
-	var temp = function($) {
+	}
+	
+	var get_keywords = function(){
+		var template = '<tr class="row[:id:] keyword_row">'+
+							'<td style="width: 179px">'+
+								'<span class="static term">[:term:]</span>'+
+								'<span class="editable"><input type="text" class="term" value="[:term:]"/></span>'+
+							'</td>'+
+							'<td style="width: 56px">'+
+								'<span class="static type">[:type:]</span>'+
+								'<span class="editable"><input type="text" class="type" value="[:type:]"/></span>'+
+							'</td>'+
+							'<td style="width: 43px">'+
+								'<span class="static relevance">[:relevance:]</span>'+
+								'<span class="editable"><input type="text" class="relevance" value="[:relevance:]"/></span>'+
+							'</td>'+
+							'<td style="width: 57px">[:source:]</td>'+
+							'<td style="width: 46px" class="buttons">'+
+								'<span class="static">'+
+									'<a href="#" onclick="peweproxy.modules.keywords.keywords_edit(\'[:id:]\'); return false"><img src="'+peweproxy_keywords_htdocs_dir+'/edit_icon.png" alt="edit" /></a>'+
+								'</span>'+
+								'<span class="editable">'+
+									'<a href="#" onclick="peweproxy.modules.keywords.keywords_save(\'[:id:]\'); return false;"><img src="'+peweproxy_keywords_htdocs_dir+'/ok_icon.png" alt="save" /></a>'+
+								'</span>'+
+								'<a href="#" onclick="peweproxy.modules.keywords.keywords_delete(\'[:id:]\'); return false;"><img src="'+peweproxy_keywords_htdocs_dir+'/delete_icon.png" alt="delete" /></a>'+
+							'</td>'+
+						'</tr>';
 		$.get(peweproxy_url_keywords+'?action=getKeyWords',function(data){
 			keywords = eval('('+data+')');
 			table = $('div#peweproxy_keywords_content table');
@@ -127,5 +122,6 @@ function peweproxy_get_keywords(){
 			});
 			$('div#peweproxy_keywords_content table tr:even').addClass('odd');
 		})
-	} (adaptiveProxyJQuery);
-}
+	}
+
+});
